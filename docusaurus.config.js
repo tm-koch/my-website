282261c5bfe7552a5454988aad942b849f377752
Thomas Koch <thomasmkoch@bluewin.ch>
2020-05-02 23:05:48 +0200

    Another fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,13 +86,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/tm-koch/my-website/edit/master/website/',
+            'https://github.com/tm-koch/my-website/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/tm-koch/my-website/edit/master/website/blog/',
+            'https://github.com/tm-koch/my-website/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
